acaf6171274250eede9b29c7d0c0cb7c7b275b3c
Badri-Oussama <98612599+Badri-Oussama@users.noreply.github.com>
2023-02-15 19:53:15 +0100

    second commit

--- src/main/java/com/example/forum_edutopia/Service/ForumService.java
+++ src/main/java/com/example/forum_edutopia/Service/ForumService.java
@@ -4,12 +4,10 @@ import com.example.forum_edutopia.Repository.CommentRepository;
 import com.example.forum_edutopia.Repository.PostRepository;
 import com.example.forum_edutopia.Repository.UpvoteRepository;
 import com.example.forum_edutopia.Repository.UserRepository;
-import com.example.forum_edutopia.entities.Post;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.Instant;
 
 
 @Service
@@ -24,7 +22,6 @@ import java.time.Instant;
         UpvoteRepository upvoteRepository;
         @Autowired
         UserRepository userRepository;
-
 
        /* public Post addPost(Post post) {
             post.setCreatedAt(Instant.now());
